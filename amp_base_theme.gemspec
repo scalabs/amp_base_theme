b5cef93a6764e9af128443e01ea8433284d8d514
Lukas Himsel <lukas@himsel.me>
2020-08-01 13:31:23 +0200

    fix amp_type

--- amp_base_theme.gemspec
+++ amp_base_theme.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |spec|
   spec.name          = "amp_base_theme"
-  spec.version       = "0.0.6"
+  spec.version       = "0.0.8"
   spec.authors       = ["Lukas Himsel"]
   spec.email         = ["lukas@himsel.me"]
 
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-gem build amp_base_theme && sudo gem install amp_base_theme-0.0.6.gem && gem contents amp_base_theme --version "=0.0.6" && gem push amp_base_theme-0.0.6.gem
+gem build amp_base_theme && sudo gem install amp_base_theme-0.0.8.gem && gem contents amp_base_theme --version "=0.0.8" && gem push amp_base_theme-0.0.8.gem
